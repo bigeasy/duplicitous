98298ff96237cee25cfcdfcdf099e4225211bc10
Alan Gutierrez <alan@prettyrobots.com>
2019-09-11 22:43:31 -0500

    Now it works, becuase log is synchronous?
    See #5.

--- test/duplicitous.test.js
+++ test/duplicitous.test.js
@@ -10,13 +10,13 @@ describe('duplicitous', () => {
         assert.equal(duplicitous.read(20), null, 'nothing')
         const inward = once(duplicitous, 'readable').promise
         input.write(Buffer.from('x'))
-        console.log('await inward')
+        // console.log('await inward')
         await inward
         assert.equal(duplicitous.read(20), null, 'still nothing')
         assert.equal(duplicitous.read(1).toString(), 'x', 'read')
         const outward = once(output, 'readable').promise
         duplicitous.write(Buffer.from('o'))
-        console.log('await outward')
+        // console.log('await outward')
         await outward
         assert.equal(output.read(1), 'o', 'written')
         console.log('done')
