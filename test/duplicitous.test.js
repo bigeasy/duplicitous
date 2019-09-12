b8c0020fdafc3bbbbb3f650d174670b168c94cec
Alan Gutierrez <alan@prettyrobots.com>
2019-09-11 22:15:51 -0500

    Debug hang in GitHub Actions.
    See #5.

--- test/duplicitous.test.js
+++ test/duplicitous.test.js
@@ -8,13 +8,14 @@ describe('duplicitous', () => {
         const output = new stream.PassThrough
         const duplicitous = new Duplicitous(input, output)
         assert.equal(duplicitous.read(20), null, 'nothing')
-        const readable = once(duplicitous, 'readable').promise
+        const inward = once(duplicitous, 'readable').promise
         input.write(Buffer.from('x'))
-        await readable
+        await inward
         assert.equal(duplicitous.read(20), null, 'still nothing')
         assert.equal(duplicitous.read(1).toString(), 'x', 'read')
+        const outward = once(output, 'readable').promise
         duplicitous.write(Buffer.from('o'))
-        await once(output, 'readable').promise
+        await outward
         assert.equal(output.read(1), 'o', 'written')
     })
 })
